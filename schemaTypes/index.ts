6e2ba53904f5188f0b31107a7cb5519d50c7c9b9
Philly-Archie <olowophilly77@gmail.com>
2025-08-20 14:24:29 +0300

    fx:deleted images structure tab

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,5 +1,4 @@
 import Events from './Events'
 import EventType from './EventType'
-import Images from './Images'
 
-export const schemaTypes = [Events, EventType, Images]
+export const schemaTypes = [Events, EventType]
